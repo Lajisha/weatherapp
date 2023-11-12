a965bc056023502c2dc52216d37d8f989a23af1d
Lajisha <Lajishab8631@gmail.com>
2023-11-12 13:40:37 +0530

    first commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -13,8 +13,8 @@ const WeatherApp = () => {
 
 
   // do api_key set up
-
-
+  const api_key = "1d0c8b16e915748055465ac0df20345f"
+  
   const [wicon,setWicon]=useState(cloud);
 
   const searchfn =async ()=>{
